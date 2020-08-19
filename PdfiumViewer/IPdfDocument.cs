5855d9f2ff3166ee0d2b4586878f9683297be6b5
daihy <dai875939260@163.com>
2020-08-19 14:31:27 +0800

    Init commit

---
+++ PdfiumViewer/IPdfDocument.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+
+namespace PdfiumViewer
+{
+    /// <summary>
+    /// Represents a PDF document.
+    /// </summary>
+    public interface IPdfDocument : IDisposable
+    {
+        /// <summary>
+        /// Number of pages in the PDF document.
+        /// </summary>
+        int PageCount { get; }
+
+        /// <summary>
+        /// Size of each page in the PDF document.
+        /// </summary>
+        IList<SizeF> PageSizes { get; }
+
+        /// <summary>
+        /// Renders a page of the PDF document to the provided graphics instance.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="graphics">Graphics instance to render the page on.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="bounds">Bounds to render the page in.</param>
+        /// <param name="forPrinting">Render the page for printing.</param>
+        void Render(int page, Graphics graphics, float dpiX, float dpiY, Rectangle bounds, bool forPrinting);
+
+        /// <summary>
+        /// Renders a page of the PDF document to the provided graphics instance.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="graphics">Graphics instance to render the page on.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="bounds">Bounds to render the page in.</param>
+        /// <param name="flags">Flags used to influence the rendering.</param>
+        void Render(int page, Graphics graphics, float dpiX, float dpiY, Rectangle bounds, PdfRenderFlags flags);
+
+        /// <summary>
+        /// Renders a page of the PDF document to an image.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="forPrinting">Render the page for printing.</param>
+        /// <returns>The rendered image.</returns>
+        Image Render(int page, float dpiX, float dpiY, bool forPrinting);
+
+        /// <summary>
+        /// Renders a page of the PDF document to an image.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="flags">Flags used to influence the rendering.</param>
+        /// <returns>The rendered image.</returns>
+        Image Render(int page, float dpiX, float dpiY, PdfRenderFlags flags);
+
+        /// <summary>
+        /// Renders a page of the PDF document to an image.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="width">Width of the rendered image.</param>
+        /// <param name="height">Height of the rendered image.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="forPrinting">Render the page for printing.</param>
+        /// <returns>The rendered image.</returns>
+        Image Render(int page, int width, int height, float dpiX, float dpiY, bool forPrinting);
+
+        /// <summary>
+        /// Renders a page of the PDF document to an image.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="width">Width of the rendered image.</param>
+        /// <param name="height">Height of the rendered image.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="flags">Flags used to influence the rendering.</param>
+        /// <returns>The rendered image.</returns>
+        Image Render(int page, int width, int height, float dpiX, float dpiY, PdfRenderFlags flags);
+
+        /// <summary>
+        /// Renders a page of the PDF document to an image.
+        /// </summary>
+        /// <param name="page">Number of the page to render.</param>
+        /// <param name="width">Width of the rendered image.</param>
+        /// <param name="height">Height of the rendered image.</param>
+        /// <param name="dpiX">Horizontal DPI.</param>
+        /// <param name="dpiY">Vertical DPI.</param>
+        /// <param name="rotate">Rotation.</param>
+        /// <param name="flags">Flags used to influence the rendering.</param>
+        /// <returns>The rendered image.</returns>
+        Image Render(int page, int width, int height, float dpiX, float dpiY, PdfRotation rotate, PdfRenderFlags flags);
+
+        /// <summary>
+        /// Save the PDF document to the specified location.
+        /// </summary>
+        /// <param name="path">Path to save the PDF document to.</param>
+        void Save(string path);
+
+        /// <summary>
+        /// Save the PDF document to the specified location.
+        /// </summary>
+        /// <param name="stream">Stream to save the PDF document to.</param>
+        void Save(Stream stream);
+
+        /// <summary>
+        /// Creates a <see cref="PrintDocument"/> for the PDF document.
+        /// </summary>
+        /// <returns></returns>
+        PrintDocument CreatePrintDocument();
+
+        /// <summary>
+        /// Creates a <see cref="PrintDocument"/> for the PDF document.
+        /// </summary>
+        /// <param name="printMode">Specifies the mode for printing. The default
+        /// value for this parameter is CutMargin.</param>
+        /// <returns></returns>
+        PrintDocument CreatePrintDocument(PdfPrintMode printMode);
+
+        /// <summary>
+        /// Creates a <see cref="PrintDocument"/> for the PDF document.
+        /// </summary>
+        /// <param name="settings">The settings used to configure the print document.</param>
+        /// <returns></returns>
+        PrintDocument CreatePrintDocument(PdfPrintSettings settings);
+
+        /// <summary>
+        /// Delete the page from the PDF document.
+        /// </summary>
+        /// <param name="page">The page to delete.</param>
+        void DeletePage(int page);
+
+        /// <summary>
+        /// Rotate the page.
+        /// </summary>
+        /// <param name="page">The page to rotate.</param>
+        /// <param name="rotation">How to rotate the page.</param>
+        void RotatePage(int page, PdfRotation rotation);
+
+        /// <summary>
+        /// Get metadata information from the PDF document.
+        /// </summary>
+        /// <returns>The PDF metadata.</returns>
+        PdfInformation GetInformation();
+
+        /// <summary>
+        /// Convert a point from device coordinates to page coordinates.
+        /// </summary>
+        /// <param name="page">The page number where the point is from.</param>
+        /// <param name="point">The point to convert.</param>
+        /// <returns>The converted point.</returns>
+        PointF PointToPdf(int page, Point point);
+
+        /// <summary>
+        /// Convert a point from page coordinates to device coordinates.
+        /// </summary>
+        /// <param name="page">The page number where the point is from.</param>
+        /// <param name="point">The point to convert.</param>
+        /// <returns>The converted point.</returns>
+        Point PointFromPdf(int page, PointF point);
+
+        /// <summary>
+        /// Convert a rectangle from device coordinates to page coordinates.
+        /// </summary>
+        /// <param name="page">The page where the rectangle is from.</param>
+        /// <param name="rect">The rectangle to convert.</param>
+        /// <returns>The converted rectangle.</returns>
+        RectangleF RectangleToPdf(int page, Rectangle rect);
+
+        /// <summary>
+        /// Convert a rectangle from page coordinates to device coordinates.
+        /// </summary>
+        /// <param name="page">The page where the rectangle is from.</param>
+        /// <param name="rect">The rectangle to convert.</param>
+        /// <returns>The converted rectangle.</returns>
+        Rectangle RectangleFromPdf(int page, RectangleF rect);
+    }
+}
